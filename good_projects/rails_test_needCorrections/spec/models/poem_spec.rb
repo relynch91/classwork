b96e19d01d6d21bab8879701242b6d63fe2c3ba4
Ryan Lynch <ReLynch91@gmail.com>
2020-05-19 09:58:56 -0700

    "update"
    update

--- good_projects/rails_test_needCorrections/spec/models/poem_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Poem, type: :model do
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:stanzas) }
-  it { should allow_value(true, false).for(:complete) }
-  it { should belong_to(:author) }
-end
--- good_projects/rails_test_needCorrections/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.4/ext/nokogiri/tmp/x86_64-apple-darwin19/ports/libxml2/2.9.9/libxml2-2.9.9/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/Users/ryan/Desktop/ryan_lynch_rails2 4/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.4/ports/x86_64-apple-darwin19/libxml2/2.9.9/lib"
-XML2_LIBS="-lxml2 -lz  -lpthread  -liconv  -lm "
-XML2_INCLUDEDIR="-I/Users/ryan/Desktop/ryan_lynch_rails2 4/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.4/ports/x86_64-apple-darwin19/libxml2/2.9.9/include/libxml2"
-MODULE_VERSION="xml2-2.9.9"
-
